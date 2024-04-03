38593c5b6e088efa18cc03109f8f3b8b76751f74
Charly-programmeur <charlynoudjalta.com>
2024-04-03 20:23:54 +0000

    maps

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.2778404240803!2d-17.459474825049003!3d14.696873585800978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xec173bc46b201ef%3A0xb2b6ea366e36e602!2sGOMYCODE%20Dakar%20Point%20E!5e0!3m2!1sfr!2ssn!4v1712175643098!5m2!1sfr!2ssn" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
